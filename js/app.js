2b6b02f92eb9314aefa988feaa3b421821c8e2d3
monavaroqua <monavaroqua@gmail.com>
2021-02-09 13:49:10 +0200

    ht

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ while (age !== "yes" && age !== "no") {
 
 var itemorder = '';
 if (age == "yes") {
-    itemorder = '<img src="images/welcome.jpeg" width="500" heigh="400"alt/>';
+    itemorder = '<img src="https://occ-0-444-1001.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVsHQNU8AQWudbHPUa-lFujAvnrpezTBPGCZ11EHrX32hPR18lNPL9gsv1dW3f6zrJILYhoL3aAccEi9507Amd-DjxERo5mO_M2R.png?r=60b" width="500" heigh="400"alt/>';
 
 }
 else if (age == "no") {
-    itemorder = '<img src="images/permission-required.jpg" width="500" heigh="400"alt />';
+    itemorder = '<img src="https://miro.medium.com/max/2600/1*0IoZi3oGqSR22Sm-t6y6dQ.jpeg" width="500" heigh="400"alt />';
 
 }
 document.write(itemorder);
